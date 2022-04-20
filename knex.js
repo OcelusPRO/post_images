fafb355ca65074a94102dad54b35c4f1ccf17db5
OcelusPRO <ocelus1999@hotmail.com>
2022-04-20 16:01:24 +0200

    new version

--- knex.js
+++
@@ -1,9 +0,0 @@
-let config = require("./config.json")
-
-const knex = require('knex')({
-    client          : config.sql_client,
-    connection: config.database,
-    pool    :   { min: 0, max: 10 }
-});
-
-module.exports = knex;
