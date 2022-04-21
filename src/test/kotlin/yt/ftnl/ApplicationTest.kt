5647d4f0b94e3ffef195548aacd2adcce0fb5055
OcelusPRO <ocelus1999@hotmail.com>
2022-04-21 16:19:34 +0200

    clean test imports

--- src/test/kotlin/yt/ftnl/ApplicationTest.kt
+++ src/test/kotlin/yt/ftnl/ApplicationTest.kt
@@ -1,23 +1,11 @@
 package yt.ftnl
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.serialization.gson.*
-import io.ktor.server.plugins.*
-import com.github.mustachejava.DefaultMustacheFactory
-import io.ktor.server.mustache.Mustache
-import io.ktor.server.mustache.MustacheContent
-import io.ktor.http.content.*
-import io.ktor.server.http.content.*
-import io.ktor.server.sessions.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
 import yt.ftnl.plugins.*
+import kotlin.test.*
 
 class ApplicationTest {
     @Test
