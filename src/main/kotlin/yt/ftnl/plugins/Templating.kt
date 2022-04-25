f8cf625820c718fa2cc763755e385a189018b8a8
OcelusPRO <ocelus1999@hotmail.com>
2022-04-25 18:46:06 +0200

    clean imports

--- src/main/kotlin/yt/ftnl/plugins/Templating.kt
+++ src/main/kotlin/yt/ftnl/plugins/Templating.kt
@@ -2,11 +2,7 @@ package yt.ftnl.plugins
 
 import com.github.mustachejava.DefaultMustacheFactory
 import io.ktor.server.mustache.Mustache
-import io.ktor.server.mustache.MustacheContent
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
-import io.ktor.server.routing.*
 import java.io.File
 
 /**
