7fe38250a71d46e78c73d52a5eae847ff426622e
OcelusPRO <ocelus1999@hotmail.com>
2022-04-25 18:45:16 +0200

    update caching header and compression + clean imports

--- src/main/kotlin/yt/ftnl/plugins/HTTP.kt
+++ src/main/kotlin/yt/ftnl/plugins/HTTP.kt
@@ -1,22 +1,18 @@
 package yt.ftnl.plugins
 
-import io.ktor.server.plugins.*
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cachingheaders.*
 import io.ktor.server.plugins.compression.*
-import io.ktor.server.plugins.cors.*
 import io.ktor.server.plugins.defaultheaders.*
-import io.ktor.server.plugins.hsts.*
-import io.ktor.server.plugins.httpsredirect.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import yt.ftnl.CONFIG
 
 /**
  * Configure base HTTP server
  */
 fun Application.configureHTTP() {
+    /*
     install(HttpsRedirect) {
             sslPort = 443
             permanentRedirect = true
@@ -24,26 +20,33 @@ fun Application.configureHTTP() {
 
     install(HSTS) {
         includeSubDomains = true
+        maxAgeInSeconds = 10
     }
-
+*/
     install(DefaultHeaders) {
         header("Made-By", "OcelusPRO")
+        header(HttpHeaders.Server, "Kotlin Server")
     }
 
     install(Compression) {
         gzip {
-            priority = 1.0
+            matchContentType(ContentType.Text.Any)
+            priority = 0.9
+            minimumSize(1024)
+            condition { request.headers[HttpHeaders.Referrer]?.startsWith(CONFIG.webCfg.serverAdress) == true }
         }
         deflate {
-            priority = 10.0
+            matchContentType(ContentType.Text.Any)
+            priority = 1.0
             minimumSize(1024)
+            condition { request.headers[HttpHeaders.Referrer]?.startsWith(CONFIG.webCfg.serverAdress) == true }
         }
     }
     install(CachingHeaders) {
         options { _, outgoingContent ->
             when (outgoingContent.contentType?.withoutParameters()) {
-                ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = (86400) ))
                 ContentType.Text.JavaScript -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = (86400) ))
+                ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = (86400) ))
                 ContentType.Text.Html -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = (86400) ))
                 ContentType.Image.Any -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = (3600) ))
                 ContentType.Video.Any -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = (7200) ))
