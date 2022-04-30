245ce695c6742c9b22d893d1b132405522336fe5
OcelusPRO <ocelus1999@hotmail.com>
2022-04-30 14:24:12 +0200

    Setup SSL

--- src/main/kotlin/yt/ftnl/Application.kt
+++ src/main/kotlin/yt/ftnl/Application.kt
@@ -1,11 +1,19 @@
 package yt.ftnl
 
+import io.ktor.network.tls.certificates.*
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.slf4j.LoggerFactory
 import yt.ftnl.core.Configuration
 import yt.ftnl.core.database.DBManager
 import yt.ftnl.plugins.*
 import java.io.File
+import java.security.KeyPairGenerator
+import java.security.KeyStore
+import java.security.Signature
+import java.security.cert.X509Certificate
+import java.util.*
 
 /**
  * Global variable to store configuration instance
@@ -40,11 +48,49 @@ fun main(args: Array<String>) {
     CONFIG = Configuration.loadConfiguration(File(if (args.isNotEmpty()) args[0] else "./config.json"))
     DBManager(CONFIG.dbConfig)
 
-    embeddedServer(Netty, port = CONFIG.webCfg.port, host = CONFIG.webCfg.host) {
-        configureRouting()
-        configureTemplating()
-        configureSecurity()
-        configureSerialization()
-        configureHTTP()
-    }.start(wait = true)
+    val keyStoreFile = File(CONFIG.webCfg.sslConfig.keystorePath)
+    var keyStore = if (keyStoreFile.exists()) {
+        val store = KeyStore.getInstance(KeyStore.getDefaultType())!!
+        store.load(keyStoreFile.inputStream(), CONFIG.webCfg.sslConfig.keyStorePassword.toCharArray())
+        val cert = store.getCertificate(CONFIG.webCfg.sslConfig.keyAlias) as X509Certificate
+        if (cert.notAfter.before(Date())) null else  store
+    } else null
+
+    keyStore = keyStore ?: generateCertificate(
+        file = keyStoreFile,
+        keyAlias = CONFIG.webCfg.sslConfig.keyAlias,
+        keyPassword = CONFIG.webCfg.sslConfig.keyPassword,
+        jksPassword = CONFIG.webCfg.sslConfig.keyStorePassword
+    )
+
+    val keys = KeyPairGenerator.getInstance("SHA-256").generateKeyPair()
+    val sign = Signature.getInstance("SHA-256")
+    sign.initSign(keys.private)
+
+
+    val environment = applicationEngineEnvironment {
+        log = LoggerFactory.getLogger("ktor.application")
+        connector {
+            port = CONFIG.webCfg.port
+            host = CONFIG.webCfg.host
+        }
+
+        sslConnector(
+            keyStore = keyStore,
+            keyAlias = CONFIG.webCfg.sslConfig.keyAlias,
+            keyStorePassword = { CONFIG.webCfg.sslConfig.keyStorePassword.toCharArray() },
+            privateKeyPassword = { CONFIG.webCfg.sslConfig.keyPassword.toCharArray() }
+        ) {
+            port = CONFIG.webCfg.sslConfig.securePort
+            keyStorePath = keyStoreFile
+        }
+
+        module(Application::configureHTTP)
+        module(Application::configureRouting)
+        module(Application::configureTemplating)
+        module(Application::configureSecurity)
+        module(Application::configureSerialization)
+    }
+
+    embeddedServer(Netty, environment).start(wait = true)
 }
